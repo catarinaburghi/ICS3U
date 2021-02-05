c1b38206a13dfb8dadf5da653bc32d3d5b066fb8
Catarina <cf.burghi@gmail.com>
2021-02-05 11:54:53 -0500

    Java Basics Unit 1

---
+++ src/JavaBasicsUnit1/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package JavaBasicsUnit1;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World!");
+		System.out.print("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/JavaBasicsUnit1/SimplePatternInicials.java
@@ -0,0 +1,17 @@
+package JavaBasicsUnit1;
+
+public class SimplePatternInicials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" CCCCC         FFFFFFFFF    BBBBB");
+		System.out.println("C     C        F            B     B");
+		System.out.println("C              F            B     B");
+		System.out.println("C              FFFFFFFF     BBBBBB");
+		System.out.println("C              F            B     B");
+		System.out.println("C     C        F            B     B");
+		System.out.println(" CCCCC         F            BBBBBB");
+	}
+
+}
